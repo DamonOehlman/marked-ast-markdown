8d2108a8a9fe2e07fe14d2c04fc77baa87eeaf8e
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-28 16:57:35 +1100

    It writes headings

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
+var entities = require('entities');
+var times = require('whisk/times');
+
+function createChar(x) {
+  return function() {
+    return x;
+  }
+}
+
+var generators = {
+  heading: function(node) {
+    return times(node.level).map(createChar('#')).join('') + ' ' + node.text.map(writeNode) + '\n';
+  }
+};
+
+function writeNode(node) {
+  var handler = generators[node.type];
+
+  if (typeof node == 'string' || (node instanceof String)) {
+    return entities.decodeHTML(node);
+  }
+
+  if (typeof handler == 'function') {
+    return handler(node);
+  }
+
+  return '';
+}
+
 module.exports = function(ast) {
+  return ast.map(writeNode).join('');
 };
